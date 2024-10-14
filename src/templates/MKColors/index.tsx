b9a3d861548ff8213c7714981c8ec94aab226730
Mike <mikayel.tevanyan@mentorcliq.com>
2024-10-14 19:04:05 +0400

    Remove MKColors components and styles
    Deleted the entire MKColors directory including MKColorsItem, MKColorsPalette, and MKColorsWrapper components along with their associated styles. This change removes all references and files related to MKColors functionality.

--- src/templates/MKColors/index.tsx
+++
@@ -1,8 +0,0 @@
-import { MKColorsItem } from './MKColorsItem';
-import { MKColorsPalette } from './MKColorsPalette';
-import { MKColorsWrapper } from './MKColorsWrapper';
-
-export const MKColors = Object.assign(MKColorsWrapper, {
-  Item: MKColorsItem,
-  Palette: MKColorsPalette,
-});
